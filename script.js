0144bb9f007b2d8d034e1762c6ed8a67ec8d86df
cjwit <chris.witulski@gmail.com>
2020-11-17 14:04:54 -0500

    Getting closer, through color

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 import Chart from "./src/Chart.js";
 
-console.log("script");
-var chart = new Chart('Cases, from 100th reported', { top: 30, right: 50, bottom: 320, left: 60 });
+window.Chart = Chart;
 
 // /*
 //  *
@@ -11,29 +10,6 @@ var chart = new Chart('Cases, from 100th reported', { top: 30, right: 50, bottom
 
 
 
-// // Set the ranges
-// var x = d3.scale.linear().range([0, width]);
-// var y = d3.scale.log().range([height, 0]);
-
-// // build tooltips
-// var tooltip = d3.select('body')
-//   .append('div')
-//   .attr('class', 'tooltip')
-//   .style('opacity', 0);
-
-// var showTooltip = function (current) {
-//   tooltip.transition()
-//     .duration(100)
-//     .style('opacity', .9);
-
-//   tooltip.html(function () {
-//     var lastCase = Number(current.cases[current.cases.length - 1]);
-//     var scaled = Math.round(lastCase * 100000.0 / current.population).toLocaleString();
-//     return '<b>' + current.name + '</b> ' + lastCase.toLocaleString() + ' total cases</br> ' + scaled + ' per 100,000k';
-//   })
-//     .style('left', (d3.event.pageX - 150) + 'px')
-//     .style('top', (d3.event.pageY - 60) + 'px');
-// }
 
 // // Get the data
 // d3.json('data.json', function (error, data) {
@@ -72,12 +48,6 @@ var chart = new Chart('Cases, from 100th reported', { top: 30, right: 50, bottom
 //  * from script.js
 //  */
 
-
-// // Define the line (linear)
-// var pathFunctionLinear = d3.svg.line()
-//     .x(function(d, i) { return x(i); })
-//     .y(function(d) { return y(d); });
-
 // // Define the line (time)
 // var pathFunctionTime = d3.svg.line()
 //     .x(function(d) { return x(d.date); })
@@ -86,13 +56,6 @@ var chart = new Chart('Cases, from 100th reported', { top: 30, right: 50, bottom
 // // set the color scale
 // var color = d3.scale.category10();
 
-// // handle mouseover and click events
-// var setActive = function(name, value) {
-//     d3.select('#label' + name.replace(/\s+/g, ''))
-//         .classed('active', value);
-//     d3.select('#line' + name.replace(/\s+/g, ''))
-//         .classed('active', value);
-// }
 
 // var highlight = function(current) {
 //     setActive(current.name, true);
@@ -107,72 +70,4 @@ var chart = new Chart('Cases, from 100th reported', { top: 30, right: 50, bottom
 //     if (!current.active) {
 //         setActive(current.name, false);
 //     }
-// }
-
-// // build graph elements
-// var addAxes = function(x, y) {
-
-//     // Define the axis
-//     var xAxis = d3.svg.axis()
-//         .scale(x)
-//         .orient('bottom')
-//         .ticks(5);
-
-//     var yAxis = d3.svg.axis()
-//         .scale(y)
-//         .orient('left')
-//         .tickSize(-width, 0, 0)
-//         .tickFormat(function(d) { return y.tickFormat(5, d3.format(',d'))(d) })
-//         .ticks(5);
-
-//     // Add axis
-//     svg.append('g')
-//         .attr('class', 'x axis')
-//         .attr('transform', 'translate(0,' + height + ')')
-//         .call(xAxis);
-
-//     svg.append('g')
-//         .attr('class', 'y axis')
-//         .call(yAxis)
-// }
-
-
-// // build graph data lines
-// var buildPaths = function(d, dataset, dataFunction) {
-//     svg.append('path')
-//         .attr('class', 'line')
-//         .style('stroke', function() {
-//             if (d.name != 'US Average') {
-//                 return d.color = color(d.name);
-//             }
-//             // set US Average in CSS
-//             return;
-//         })
-//         .attr('id', 'line' + d.name.replace(/\s+/g, ''))
-//         .attr('d', dataFunction(dataset))
-//         .on('mouseover', function() { highlight(d); })
-//         .on('mouseout', function() { removeHighlight(d); });
-// }
-
-// // build legend and set event handlers
-// var buildLegend = function(d, i) {
-//     legendWidthSpacing = width / 4
-//     d.active = false
-//     svg.append('text')
-//         .attr('x', (legendWidthSpacing / 2 + Math.floor(i / 14) * legendWidthSpacing))
-//         .attr('y', height + 40 + (i % 14 * 20))
-//         .attr('class', 'legend')
-//         .attr('id', 'label' + d.name.replace(/\s+/g, ''))
-//         .style('fill', function() { return d.color = color(d.name); })
-//         .text(d.name)
-//         .on('mouseover', function() {
-//             if (!d.active) { setActive(d.name, true); }
-//         })
-//         .on('mouseout', function() {
-//             if (!d.active) { setActive(d.name, false); }
-//         })
-//         .on('click', function() {
-//             if (!d.active) { setActive(d.name, true); } else { setActive(d.name, false); }
-//             d.active = !d.active
-//         });
 // }
--- src/Chart.js
+++ src/Chart.js
@@ -2,7 +2,6 @@ import "./d3.min.js";
 
 export default class Chart {
   constructor(title, margin) {
-    console.log("constructor");
 
     // set the dimensions of the chart canvas
     this.width = 800 - margin.left - margin.right;
@@ -11,6 +10,20 @@ export default class Chart {
     // create svg element    
     this.svg = this.createSVG(margin);
     this.createChartTitle(title)
+
+    // set ranges
+    this.x = d3.scale.linear().range([0, this.width]);
+    this.y = d3.scale.log().range([this.height, 0]);
+
+    // build tooltips
+    this.tooltip = d3.select('body')
+      .append('div')
+      .attr('class', 'tooltip')
+      .style('opacity', 0);
+
+    // get data from JSON
+    this.addData();
+
     return this.svg;
   }
 
@@ -33,5 +46,171 @@ export default class Chart {
       .text(title);
 
     return;
+  }
+
+  // specific to the graph... needs changed
+  showTooltip(current) {
+    this.tooltip.transition()
+      .duration(100)
+      .style('opacity', .9);
+
+    this.tooltip.html(function () {
+      var lastCase = Number(current.cases[current.cases.length - 1]);
+      var scaled = Math.round(lastCase * 100000.0 / current.population).toLocaleString();
+      return '<b>' + current.name + '</b> ' + lastCase.toLocaleString() + ' total cases</br> ' + scaled + ' per 100,000k';
+    })
+      .style('left', (d3.event.pageX - 150) + 'px')
+      .style('top', (d3.event.pageY - 60) + 'px');
+  }
+
+  async addData() {
+    this.data = await this.getData();
+    console.log('addData', this.data);
+
+        // work into functions
+    // Dynamically scale the range of the data using Washington (where first cases appear)
+    var daysOver100 = this.data.states['Washington'].cases.filter(
+      function (n) { return n >= 100; })
+      .length
+    this.x.domain([0, daysOver100]);
+    this.y.domain([100, this.data.maxCases]);
+
+    this.addAxes();
+
+    // sort data and loop through each state
+    var stateData = Object.entries(this.data.states).sort(
+      function (a, b) { return a[0] > b[0]; }
+    );
+    
+    var self = this;
+    stateData.forEach(function (d, i) {
+      // get object from iterator array
+      d = d[1];
+
+      // filter cases to show only 100+
+      d.cases = d.cases.filter(function (num) { return num >= 100; });
+
+      // build the graph line and legend
+      self.buildPaths(d, d.cases);
+      self.buildLegend(d, i);
+    });
+
+  }
+
+  async getData() {
+    return new Promise((resolve, reject) => {
+      d3.json('data.json', function (error, data) {
+        delete data.states["US Average"];
+        resolve(data);
+      });
+    })
+  }
+    
+
+
+  // from script
+  addAxes() {
+    // Define the axis
+    var xAxis = d3.svg.axis()
+      .scale(this.x)
+      .orient('bottom')
+      .ticks(5);
+
+    var self = this;
+    var yAxis = d3.svg.axis()
+      .scale(this.y)
+      .orient('left')
+      .tickSize(-this.width, 0, 0)
+      .tickFormat(function (d) { return self.y.tickFormat(5, d3.format(',d'))(d) })
+      .ticks(5);
+
+    // Add axis
+    this.svg.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', 'translate(0,' + this.height + ')')
+      .call(xAxis);
+
+    this.svg.append('g')
+      .attr('class', 'y axis')
+      .call(yAxis)
+  }
+
+  // from script
+  buildPaths(d, dataset) {
+    var self = this;
+    this.svg.append('path')
+      .attr('class', 'line')
+      .style('stroke', function () {
+        if (d.name != 'US Average') {
+          return d.color = this.color(d.name);
+        }
+        // set US Average in CSS
+        return;
+      })
+      .attr('id', 'line' + d.name.replace(/\s+/g, ''))
+      .attr('d', this.pathDataFunction(dataset))
+      .on('mouseover', function () { self.highlight(d); })
+      .on('mouseout', function () { self.removeHighlight(d); });
+  }
+
+  // from script, linear, should overwrite in time charts
+  pathDataFunction(d) {
+    var self = this;
+    d3.svg.line()
+      .x(function (d, i) { return self.x(i); })
+      .y(function (d) { return self.y(d); });
+  }
+
+  // from script
+  buildLegend(d, i) {
+    var legendWidthSpacing = width / 4
+    d.active = false
+    this.svg.append('text')
+      .attr('x', (legendWidthSpacing / 2 + Math.floor(i / 14) * legendWidthSpacing))
+      .attr('y', this.height + 40 + (i % 14 * 20))
+      .attr('class', 'legend')
+      .attr('id', 'label' + d.name.replace(/\s+/g, ''))
+      .style('fill', function () { return d.color = this.color(d.name); })
+      .text(d.name)
+      .on('mouseover', function () {
+        if (!d.active) { this.setActive(d.name, true); }
+      })
+      .on('mouseout', function () {
+        if (!d.active) { this.setActive(d.name, false); }
+      })
+      .on('click', function () {
+        if (!d.active) { this.setActive(d.name, true); } else { this.setActive(d.name, false); }
+        d.active = !d.active
+      });
+  }
+
+  // from script
+  color(name) {
+    return d3.scale.category10()(name);
+  }
+
+  // from script
+  setActive(name, value) {
+    d3.select('#label' + name.replace(/\s+/g, ''))
+      .classed('active', value);
+    d3.select('#line' + name.replace(/\s+/g, ''))
+      .classed('active', value);
+  }
+
+  // from script
+  highlight(current) {
+    this.setActive(current.name, true);
+    this.showTooltip(current);
+  }
+
+  // from script
+  removeHighlight(current) {
+    this.tooltip.transition()
+      .duration(100)
+      .style('opacity', 0);
+
+    if (!current.active) {
+      this.setActive(current.name, false);
+    }
   }
 }
