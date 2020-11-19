6690559af74aeedcc67d1fc842e57eedd15727b9
cjwit <chris.witulski@gmail.com>
2020-11-18 20:54:34 -0500

    Daily averages works

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
-import CasesChart from "./src/CasesChart.js";0
+import CasesChart from "./src/CasesChart.js";
 import CasesScaledChart from "./src/CasesScaledChart.js";
 import DeathsChart from "./src/DeathsChart.js";
+import DailyChart from "./src/DailyChart.js";
 
 window.CasesChart = CasesChart;
 window.CasesScaledChart = CasesScaledChart;
 window.DeathsChart = DeathsChart;
+window.DailyChart = DailyChart;
 
 /**
  * 
-      var showTooltip = function (current) {
-
 
       // Get the data
-      d3.json('data.json', function (error, data) {
+      d3.json("data.json", function (error, data) {
+        delete data.states["US Average"];
+        
+        // Dynamically scale the range of the data
+        var dayOne = new Date(2020, 0, 21);
+        var yesterday = new Date(Date.now() - 24 * 60 * 60 * 1000);
+        x.domain([dayOne, yesterday]);
+        y.domain([100, data.maxAverages]);
 
+        addAxes(x, y);
 
+        // sort data and loop through each state
+        var stateData = Object.entries(data.states).sort(
+          function (a, b) { return a[0] > b[0]; }
+        );
 
-          // build the graph line and legend
+        stateData.forEach(function (d, i) {
+          // get object from iterator array
+          d = d[1];
+
+          // add dates for x values
+          var pairs = [];
+          for (var day = 0; day < data.totalDays; day++) {
+            let currentDay = new Date(dayOne.valueOf() + day * 24 * 60 * 60 * 1000);
+            pairs.push({ "date": currentDay, "y": d.averages[day] });
+          };
+
           buildPaths(d, pairs, pathFunctionTime);
-
-          // adjust i for buildLegend after US Average
-          if (d.name < 'US Average') {
-            buildLegend(d, i);
-          }
-          else if (d.name > 'US Average') {
-            buildLegend(d, i - 1);
-          }
+          buildLegend(d, i);
         });
       });
+
 
 
  */
---
+++ src/DailyChart.js
@@ -0,0 +1,98 @@
+import "./d3.min.js";
+import { Chart } from "./Chart.js";
+
+
+export default class DailyChart extends Chart {
+  constructor(title, margin) {
+    super(title, margin);
+  }
+
+  setRanges() {
+    this.x = d3.time.scale().range([0, this.width]);
+    this.y = d3.scale.linear().range([this.height, 0]);
+    this.color = d3.scale.category10();
+  }
+
+  setDomain() {
+    // Dynamically scale the range of the data 
+    this.dayOne = new Date(2020, 0, 21);
+    var yesterday = new Date(Date.now() - 24 * 60 * 60 * 1000);
+    this.x.domain([this.dayOne, yesterday]);
+    this.y.domain([0, this.data.maxAverages]);
+  }
+
+  async getData() {
+    return new Promise((resolve, reject) => {
+      d3.json('assets/data.json', function (error, data) {
+        delete data.states["US Average"];
+        resolve(data);
+      });
+    })
+  }
+
+  filterStates(stateDataObject) {
+    // filter cases to show only 100
+    stateDataObject.cases = stateDataObject.cases
+      .filter(function (num) { return num >= 100; });
+    return stateDataObject;
+  }
+
+  // from script
+  buildPath(d, i) {
+
+    // setup variables for anonymous functions
+    var x = this.x;
+    var y = this.y;
+
+    // linear graph, needs changing for time axis
+    var pathFunction = d3.svg.line()
+      .x(function (d) { return x(d.date); })
+      .y(function (d) { return d.y > 0 ? y(d.y) : y(0) }); // fixing negative cases in Massachusets
+
+    var pairs = this.createDatePairs(d);
+    this.appendPath(d, pairs, pathFunction);
+  }
+
+  createDatePairs(d) {
+    var pairs = [];
+    for (var day = 0; day < this.data.totalDays; day++) {
+      let currentDay = new Date(this.dayOne.valueOf() + day * 24 * 60 * 60 * 1000);
+      pairs.push({ "date": currentDay, "y": d.averages[day] });
+    };
+    return pairs;
+  }
+
+  appendPath(d, pairs, pathFunction) {
+    var self = this;
+
+    this.svg.append('path')
+      .attr('class', 'line')
+      .style('stroke', function () {
+        if (d.name != 'US Average') {
+          return d.color = self.color(d.name);
+        }
+        // set US Average in CSS
+        return;
+      })
+      .attr('id', 'line' + d.name.replace(/\s+/g, ''))
+      // .attr('d', this.pathDataFunction(dataset))
+      .attr('d', pathFunction(pairs))
+      .on('mouseover', function () { self.highlight(d); })
+      .on('mouseout', function () { self.removeHighlight(d); });
+  }
+
+  showTooltip(current) {
+    this.tooltip.transition()
+      .duration(100)
+      .style('opacity', .9);
+
+    this.tooltip.html(function () {
+      return "<b>" + current.name +
+        "</b> max: " + Math.round(current.maxAverages).toLocaleString() +
+        " current: " + Math.round(Number(current.averages[current.averages.length - 1])).toLocaleString() +
+        "<br>yesterday's cases: " + (current.cases[current.cases.length - 1] - current.cases[current.cases.length - 2]).toLocaleString();
+    })
+      .style('left', (d3.event.pageX - 150) + 'px')
+      .style('top', (d3.event.pageY - 60) + 'px');
+  }
+}
